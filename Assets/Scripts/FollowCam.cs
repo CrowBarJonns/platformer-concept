1ded88161f61d85fc2796041fa2ba90355ccb140
CrowBarJonns <macalite@flashgiver.com>
2021-07-09 08:47:32 +1200

    Simple Platformer Game
    Simple game created to learn the basic concepts of a 2D Platformer.

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float smoothTime = 0.2f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetPosition = new Vector3(
+            target.position.x, target.position.y, transform.position.z);
+
+        transform.position = Vector3.SmoothDamp(transform.position,
+            targetPosition, ref velocity, smoothTime);
+    }
+}
